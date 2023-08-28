a9face64baf38ae9a969ffcf31704c1935bdde63
leon-rdo <leonardoabreu1212@gmail.com>
2023-08-28 13:43:46 -0300

    refactor: simplifica lógica de votação

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,5 @@ from .views import *
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('pergunta/<int:pk>/', QuestionView.as_view(), name='pergunta'),
-    path('pergunta/<int:pk>/vote/', vote, name='vote'),
     path('pergunta/<int:pk>/resultados/', ResultsView.as_view(), name='resultados'),
 ]
